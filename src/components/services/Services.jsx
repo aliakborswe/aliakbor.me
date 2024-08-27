a5bc8f5d29f3653ebd9fee13058a3c4f2e3030af
mdaliakbor <aliakbor.js@gmail.com>
2024-08-27 23:11:23 +0600

    service icon add

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -3,6 +3,9 @@ import Container from "../common/container/Container";
 import Title from "../common/title/Title";
 import Image from "next/image";
 import "./services.css"
+import webdesign from "@/assets/svg/webdesign.svg";
+import webdevelopment from "@/assets/svg/webdevelopment.svg";
+import backenddevelopment from "@/assets/svg/backenddevelopment.svg";
 
 const Services = () => {
   return (
@@ -11,7 +14,7 @@ const Services = () => {
         <Title side='right' title='Services' height='160px' />
         <div className='services-container'>
           <div className='service'>
-            <Image src='' alt='' />
+            <Image src={webdesign} width={80} height={80} alt='' />
             <h4>Responsive Web Design</h4>
             <p>
               Responsive Web Design ensures your website looks and functions
@@ -22,7 +25,7 @@ const Services = () => {
             </p>
           </div>
           <div className='service'>
-            <Image src='' alt='' />
+            <Image src={webdevelopment} width={80} height={80} alt='' />
             <h4>Frontend Development</h4>
             <p>
               As a MERN stack developer, I offer specialized frontend
@@ -31,7 +34,7 @@ const Services = () => {
             </p>
           </div>
           <div className='service'>
-            <Image src='' alt='' />
+            <Image src={backenddevelopment} width={80} height={80} alt='' />
             <h4>Backend Development</h4>
             <p>
               I'm a backend developer with expertise in Node.js, Express, and
